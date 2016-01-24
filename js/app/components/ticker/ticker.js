61d373bf7f6396c12b5f47e1e2ff0f036089c156
Adam Faryna <adamfaryna@gmail.com>
2016-01-24 22:57:02 +0000

    code cleanup

--- js/app/components/ticker/ticker.js
+++ js/app/components/ticker/ticker.js
@@ -7,7 +7,7 @@ app.directive('ticker', ['$interval', '$timeout', 'componentsPath', 'sportIconMa
     link: function(scope, elm, attrs) {
     	var url = 'http://api.unicdn.net/v1/feeds/sportsbook/event/live.jsonp?app_id=ca7871d7&app_key=5371c125b8d99c8f6b5ff9a12de8b85a';
       var fetchInterval = attrs.fetchInterval || 120000;
-      scope.betLinkFormat = 'https://www.unibet.com/betting#/event/live';
+      scope.betLinkFormat = 'https://www.unibet.com/betting#/event/live/';
 
       var carouselOptions = {
         autoplay: true,
--- js/app/components/timeFilter/timeFilter.js
+++ js/app/components/timeFilter/timeFilter.js
@@ -4,11 +4,12 @@ app.filter('timeFilter', [function() {
 	return function(x) {
 		var date = x.slice(0, 10);
 		var time = x.slice(-6, -1);
+		
 		var xDate = new Date(Date.parse(x));
+		xDate.setHours(0, 0, 0, 0);
 
 		var today = new Date();
 		today.setHours(0, 0, 0, 0);
-		xDate.setHours(0, 0, 0, 0);
 
 		if (xDate.getTime() == today.getTime()) {
 			date = 'Today';
