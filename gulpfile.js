1a41e77ee8c6a23aa09880586f2688e586efbb28
Adam Faryna <adamfaryna@gmail.com>
2016-02-10 00:36:53 +0000

    + almost working

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp         = require('gulp');
 var browserSync  = require('browser-sync').create();
 var $ = require('gulp-load-plugins')();
 
+var lessFiles = ['less/*.less', 'js/app/**/*.less'];
+
 gulp.task('lint', function() {
   return gulp.src(['app/**/*.js', 'gulpfile.js'])
     .pipe($.jshint('.jshintrc'))
@@ -9,14 +11,27 @@ gulp.task('lint', function() {
 });
 
 // Static Server + watching files
-gulp.task('serve', ['lint'], function() {
+gulp.task('serve', ['less', 'lint'], function() {
   browserSync.init({
     browser: ['firefox'],
     server: '.',
     port: '3000'
   });
 
+  gulp.watch(lessFiles, ['less']);
   gulp.watch(['index.html', 'js/**/*.*']).on('change', browserSync.reload);
+});
+
+// Compile less into CSS & auto-inject into browsers
+gulp.task('less', function() {
+  return gulp.src(lessFiles)
+    .pipe($.less())
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('default', ['serve']);
--- js/app/components/ticker/ticker.js
+++ js/app/components/ticker/ticker.js
@@ -9,85 +9,91 @@ app.directive('ticker', ['$interval', '$timeout', 'componentsPath', 'sportIconMa
       var fetchInterval = attrs.fetchInterval || options.defaultServiceFetchInterval;
       scope.betLinkFormat = options.betLinkFormat;
 
-      var carouselOptions = {
-        autoplay: true,
-        autoplaySpeed: attrs.slideInterval || 3000,
-        speed: attrs.transitionDuration || 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false,
-        infinite: true,
-        cssEase: 'linear',
-        prevArrow: '',
-        nextArrow: ''
-      };
+      fetchData();
+      $interval(fetchData, fetchInterval);
 
-      $(function() {
-      	fetchData();
-        $interval(fetchData, fetchInterval);
+      var isNextPlayRunning = false;
+      //var currentPlay;
+      var carouelStarted = false;
 
-        function fetchData() {
-          $http.jsonp(options.serviceURL + '&callback=JSON_CALLBACK')
-            .then(function(response) {
-              callback(response.data);
+      function startCarousel() {
+        carouelStarted = true;
+        isNextPlayRunning = true;
+        var match = elm.find('match');
 
-            }, function(response) {
-              console.log(JSON.stringify(response));
-          });
-        }
+        function switcher() {
+          var currentOffset = parseInt(match.css('left'), 10);
+          currentOffset -= 1;
+          match.css('left', currentOffset + 'px');
 
-        function isSlickInited() {
-          return elm.find('match').hasClass('slick-initialized');
-        }
-
-        function mapSportToIcon(sportString) {
-          var pom = sportString.toUpperCase();
-
-          for (var key in sportIconMap) {
-            if (~pom.indexOf(key)) {
-              return sportIconMap[key];
-            }
+          console.log(currentOffset + ' % 585 = ' + (currentOffset % 585 === 0));
+          if (currentOffset % 585 === 0) {
+            clearInterval(timer);
+            isNextPlayRunning = false;
+            setTimeout(startCarousel, 3000);
           }
-
-          return sportIconMap.DEFAULT;
         }
 
-        function callback(data) {
-          var newPlays = [];
+        var timer = setInterval(switcher, 10);
+      }
 
-          data.liveEvents.forEach(function(elem) {
-						var play = {
-							eventId: elem.liveData.eventId,
-							score: elem.liveData.score ? elem.liveData.score.home + ' - ' + elem.liveData.score.away : '',
-							name: elem.event.name,
-							sportIcon: mapSportToIcon(elem.event.sport),
-              sportIconAlt: 'Sport Icon',
-							startTime: elem.event.start
-						};
+      function fetchData() {
+        $http.jsonp(options.serviceURL + '&callback=JSON_CALLBACK')
+          .then(function(response) {
+            callback(response.data);
 
-						newPlays.push(play);
-					});
-
-					$timeout(function() {
-						scope.plays = newPlays;
-					});
-        }
-
-        function reinitSlick() {
-					var slickElem = elm.find('match');
-        	if (isSlickInited()) {
-						slickElem.slick('unslick');
-					}
-
-					slickElem.slick(carouselOptions);
-        }
-
-        scope.$watch('plays', function (newVal) {
-        	if (newVal) {
-						reinitSlick();
-        	}
+          }, function(response) {
+            console.log(JSON.stringify(response));
         });
-      });
+      }
+
+      function mapSportToIcon(sportString) {
+        var pom = sportString.toUpperCase();
+
+        for (var key in sportIconMap) {
+          if (~pom.indexOf(key)) {
+            return sportIconMap[key];
+          }
+        }
+
+        return sportIconMap.DEFAULT;
+      }
+
+      function callback(data) {
+        var newPlays = [];
+
+        data.liveEvents.forEach(function(elem) {
+          var play = {
+            eventId: elem.liveData.eventId,
+            score: elem.liveData.score ? elem.liveData.score.home + ' - ' + elem.liveData.score.away : '',
+            name: elem.event.name,
+            sportIcon: mapSportToIcon(elem.event.sport),
+            sportIconAlt: 'Sport Icon',
+            startTime: elem.event.start
+          };
+
+          newPlays.push(play);
+        });
+
+        var singlePlayWidth = elm.find('cover').css('width');
+        var matchWidth = parseInt(singlePlayWidth, 10) * newPlays.length;
+        elm.find('match').css('width', matchWidth + 'px');
+
+        (function check() {
+          if (isNextPlayRunning) {
+            $timeout(function () {
+              scope.plays = newPlays;
+            });
+
+            if (!carouelStarted) {
+              setTimeout(startCarousel, 300);
+            }
+
+          } else {
+            setTimeout(check, 10);
+          }
+        })();
+      }
     }
   };
 }]);
